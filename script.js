949c815fcd938ac3c8acaea9b7106ab6b717207f
Nick Zart <nzart@g.clemson.edu>
2019-03-14 10:21:26 -0400

    UPDATE: 3.14.19

--- script.js
+++ script.js
@@ -1,46 +1,6 @@
 "use strict";
 
-/*
-function searchWeather() {
-    loadingText.style.display = 'block';
-    weatherBox.style.display = 'none';
-    var cityName = searchCity.value;
-    if (cityName.trim().length == 0) {
-        return alert('Please enter a City Name');
-    }
-*/
-//API DATA
-//var requestURL = 'https://api.openweathermap.org/data/2.5/weather?q=London,uk&appid=5328bb99fc309788816fd9447e0a2cc7&units=imperial';
 
-/*
-var city = document.querySelector('#city');
-function searchCity() {
-    var cityname = city.value;
-}
-var requestURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=5328bb99fc309788816fd9447e0a2cc7&units=imperial';
-
-
-var request = new XMLHttpRequest();
-request.open('GET', requestURL);
-//request.responseType = 'json';
-request.send();
-
-request.onload = function() {
-    if (request.readyState == XMLHttpRequest.DONE && request.status === 200){
-    //console.log(JSON.parse(this.responseText));
-        var data = JSON.parse(this.responseText);
-        var weatherData = data.weather[0].main;
-        var tempMax = data.main.temp_max;
-        var tempMin = data.main.temp_min;
-        displayWeather(weatherData, tempMax, tempMin);
-        //console.log(data);
-        console.log(tempMin);
-        
-    } else if (request.readyState === XMLHttpRequest.DONE) {
-        console.log('Something went wrong!');
-    }
-};
-*/
 // Display Weather    
 var cond = document.querySelector('#cond');
 var high = document.querySelector('#high');
@@ -56,27 +16,33 @@ var city = document.querySelector('#city');
 function searchCity() {
     var cityname = city.value;
 
-var requestURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityname + '&appid=5328bb99fc309788816fd9447e0a2cc7&units=imperial';
+    var requestURL = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityname + '&appid=5328bb99fc309788816fd9447e0a2cc7&units=imperial';
 
 
-var request = new XMLHttpRequest();
-request.open('GET', requestURL);
-//request.responseType = 'json';
-request.send();
+    var request = new XMLHttpRequest();
+    request.open('GET', requestURL);
+    request.send();
 
-request.onload = function() {
-    if (request.readyState == XMLHttpRequest.DONE && request.status === 200){
-    //console.log(JSON.parse(this.responseText));
-        var data = JSON.parse(this.responseText);
-        var weatherData = (cityname, data.weather[0].main);
-        var tempMax = data.main.temp_max;
-        var tempMin = data.main.temp_min;
-        searchCity();
-        displayWeather(weatherData, tempMax, tempMin);
-        //console.log(data);
-        //console.log(tempMin);
-        
-    } else if (request.readyState === XMLHttpRequest.DONE) {
-        console.log('Something went wrong!');
+    request.onreadystatechange = function() {
+        if (request.readyState == XMLHttpRequest.DONE && request.status === 200){
+            var data = JSON.parse(this.responseText);
+            var weatherData = (cityname, data.weather[0].main);
+            var tempMax = data.main.temp_max;
+            var tempMin = data.main.temp_min;
+            searchCity();
+            displayWeather(weatherData, tempMax, tempMin);
+            
+        } else if (request.readyState === XMLHttpRequest.DONE) {
+            alert('Something went wrong!');
+        }
     }
-}};
+};
+
+
+// Return event listener
+
+document.addEventListener('keypress', function(event) {
+            if (event.keyCode === 13 || event.which === 13) {
+                searchCity();
+            }
+        });
